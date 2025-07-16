646f11b7ef7a2eb896797549d6f59a68fa5666f0
arturlima25 <arturlimatrabalho@gmail.com>
2025-07-15 21:03:02 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import gspread
-from oauth2client.service_account import ServiceAccountCredentials
+from google.oauth2.service_account import Credentials
 from datetime import date
 
 # -------------------------------
@@ -9,8 +9,23 @@ from datetime import date
 # -------------------------------
 @st.cache_resource
 def conectar_sheets():
-    escopo = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
-    creds = ServiceAccountCredentials.from_json_keyfile_name('credenciais.json', escopo)
+    escopo = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
+    
+    # Pegando credenciais do secrets.toml
+    credenciais_dict = {
+        "type": st.secrets["type"],
+        "project_id": st.secrets["project_id"],
+        "private_key_id": st.secrets["private_key_id"],
+        "private_key": st.secrets["private_key"].replace("\\n", "\n"),
+        "client_email": st.secrets["client_email"],
+        "client_id": st.secrets["client_id"],
+        "auth_uri": st.secrets["auth_uri"],
+        "token_uri": st.secrets["token_uri"],
+        "auth_provider_x509_cert_url": st.secrets["auth_provider_x509_cert_url"],
+        "client_x509_cert_url": st.secrets["client_x509_cert_url"]
+    }
+
+    creds = Credentials.from_service_account_info(credenciais_dict, scopes=escopo)
     cliente = gspread.authorize(creds)
     planilha = cliente.open("FinancasDomesticas")
     aba = planilha.sheet1
